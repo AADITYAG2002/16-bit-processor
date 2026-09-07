aeae5be09fe66915913fad8dbbedf66a69d6379c
AADITYAG2002 <aadityag2002@gmail.com>
2026-09-07 04:55:56 +0000

    Updated Control module along with synchronous data transfer.

--- hardware/Control.sv
+++ hardware/Control.sv
@@ -1,51 +1,74 @@
-`define ROM_SIZE 16
-`define ROM_ADDR_SIZE 16
+`define ROM_ADDR_SIZE 32
+`define ROM_DATA_SIZE 23
+`define ROM_NUM_INST_SIZE 3
 
 module ROM (
     input  logic [`ROM_ADDR_SIZE - 1 : 0] rom_addr,
-    output logic [     `ROM_SIZE - 1 : 0] CTRL
+    output logic [`ROM_DATA_SIZE - 1 : 0] CTRL
 );
 
-    logic [`ROM_SIZE:0] memory = 0;
-    assign CTRL = memory[rom_addr];
+    parameter [`ROM_DATA_SIZE * (`ROM_NUM_INST_SIZE + 1) - 1:0] memory = {
+        // {Reg_Read, Reg_Read_Addr[2:0], Reg_Write, Reg_Write_Addr[2:0], Control_Read, Control_Write, Io_Read, IR_Read, MDR_Read, PC_Sel[2:0], ALU_In_1, ALU_In_2, ALU_Op_Sel[3:0], ALU_Out_Write}
+        `ROM_DATA_SIZE'b0_000_0_000_0_0_0_1_0_000_0_0_0000_0,
+        `ROM_DATA_SIZE'b0_000_0_000_0_0_0_0_0_000_0_0_0000_0,
+        `ROM_DATA_SIZE'b0_000_0_000_0_0_0_0_0_000_0_0_0000_0
+    };
 
+    assign CTRL = memory[`ROM_DATA_SIZE*rom_addr+:`ROM_DATA_SIZE];
 endmodule
 
 module Control #(
     parameter unsigned DATA_WIDTH = 16
 ) (
-    input  logic                      reset_n,
-    input  logic                      clk,
-    input  logic [DATA_WIDTH - 1 : 0] instruction,
-    output logic                      Reg_Write,
-    output logic                      Reg_Read,
-    output logic                      Control_Read,
-    output logic                      Control_Write,
-    output logic                      Io_Read,
-    output logic                      IR_Read,
-    output logic                      MDR_Read,
-    output logic                      PC_Sel,
-    input  logic                      Z_flag,
-    input  logic                      C_flag,
-    output logic                      ALU_In_1,
-    output logic                      ALU_In_2,
-    output logic [             3 : 0] ALU_Op_Sel,
-    output logic                      ALU_Out_Write,
-    output logic [             2 : 0] Reg_Read_Addr,
-    output logic [             2 : 0] Reg_Write_Addr,
-    output logic [DATA_WIDTH - 1 : 0] Read_Addr,
-    output logic [DATA_WIDTH - 1 : 0] Write_Addr
+    input logic                      reset_n,
+    input logic                      clk,
+    input logic [DATA_WIDTH - 1 : 0] MDR,
+    input logic [DATA_WIDTH - 1 : 0] IR,
+    input logic                      Z_flag,
+    input logic                      C_flag,
+
+    output logic         Reg_Read,
+    output logic [2 : 0] Reg_Read_Addr,
+    output logic         Reg_Write,
+    output logic [2 : 0] Reg_Write_Addr,
+
+    output logic         Control_Read,
+    output logic         Control_Write,
+    output logic         Io_Read,
+    output logic         IR_Read,
+    output logic         MDR_Read,
+    output logic [2 : 0] PC_Sel,
+
+    output logic         ALU_In_1,
+    output logic         ALU_In_2,
+    output logic [3 : 0] ALU_Op_Sel,
+    output logic         ALU_Out_Write
 );
 
     logic [`ROM_ADDR_SIZE - 1 : 0] rom_addr;
-    logic [`ROM_SIZE - 1 : 0] CTRL;
+    logic [`ROM_DATA_SIZE - 1 : 0] CTRL;
 
     ROM rom (
         .rom_addr(rom_addr),
         .CTRL    (CTRL)
     );
 
-    assign {Reg_Read_Addr, Reg_Read, Reg_Write_Addr, Reg_Write, Control_Read, Control_Write, Io_Read, IR_Read, MDR_Read, Read_Addr, Write_Addr, PC_Sel, ALU_In_1, ALU_In_2, ALU_Op_Sel} = CTRL;
+    assign {
+        Reg_Read, Reg_Read_Addr, Reg_Write, Reg_Write_Addr,
+        Control_Read, Control_Write, Io_Read, IR_Read, MDR_Read, PC_Sel,
+        ALU_In_1, ALU_In_2, ALU_Op_Sel, ALU_Out_Write
+    } = CTRL;
+
+    always_ff @(posedge clk) begin
+        if (!reset_n) rom_addr <= 0;
+
+        case (rom_addr)
+            0: begin
+                rom_addr <= 0;
+            end
+            default: rom_addr <= 0;
+        endcase
+    end
 
 endmodule
 
--- hardware/Processor.sv
+++ hardware/Processor.sv
@@ -12,8 +12,8 @@ module Processor #(
 
     ////////////////////////  Declaration & Inittialization  ///////////////////////////////
     // Registers & flags
-    logic [ADDR_WIDTH - 1 : 0] PC, SP;
-    logic [DATA_WIDTH - 1 : 0] MDR, IR;
+    reg [ADDR_WIDTH - 1 : 0] PC, SP;
+    reg [DATA_WIDTH - 1 : 0] MDR, IR;
     logic Z_flag, C_flag;
 
     // control signals
@@ -25,13 +25,12 @@ module Processor #(
         MDR_Read,
         Io_Read,
         Control_Write,
-        PC_Sel,
         ALU_In_1,
         ALU_In_2,
         ALU_Out_Write;
+    logic [2:0] PC_Sel;
     logic [3:0] ALU_Op_Sel;
     logic [2 : 0] Reg_Read_Addr, Reg_Write_Addr;
-    logic [DATA_WIDTH - 1 : 0] Read_Addr, Write_Addr;
 
     // interconnect wires & reg
     logic [DATA_WIDTH - 1 : 0] internal_databus;
@@ -74,15 +73,13 @@ module Processor #(
         .MDR_Read      (MDR_Read),
         .PC_Sel        (PC_Sel),
         .Z_flag        (Z_flag),
-        .C_flag        (Z_flag),
+        .C_flag        (C_flag),
         .ALU_In_1      (ALU_In_1),
         .ALU_In_2      (ALU_In_2),
         .ALU_Op_Sel    (ALU_Op_Sel),
         .ALU_Out_Write (ALU_Out_Write),
         .Reg_Read_Addr (Reg_Read_Addr),
         .Reg_Write_Addr(Reg_Write_Addr),
-        .Read_Addr     (Read_Addr),
-        .Write_Addr    (Write_Addr)
     );
 
     ////////////////////////////////////////////////////////////////////////////////////////
@@ -95,22 +92,22 @@ module Processor #(
     // Write to memory (via pointer in register) from a register
     assign data             = write_mem ? internal_databus : {DATA_WIDTH{1'hz}};
 
-    always_comb begin
-        if (Control_Read) begin
-            address  = Io_Read ? Read_Addr : PC;
-            read_mem = 1;
-            IR       = IR_Read ? internal_databus : {DATA_WIDTH{1'b0}};
-            MDR      = MDR_Read ? internal_databus : {DATA_WIDTH{1'b0}};
+    always_ff @(posedge clk) begin
+        if (Control_Read && !Control_Write) begin
+            address  <= Io_Read ? IR : PC;
+            read_mem <= 1;
+            IR       <= IR_Read ? internal_databus : {DATA_WIDTH{1'b0}};
+            MDR      <= MDR_Read ? internal_databus : {DATA_WIDTH{1'b0}};
         end else begin
-            address  = {DATA_WIDTH{1'hz}};
-            read_mem = 0;
+            address  <= {DATA_WIDTH{1'hz}};
+            read_mem <= 0;
         end
-        if (Control_Write) begin
-            address   = Write_Addr;
-            write_mem = 1;
+        if (Control_Write && !Control_Read) begin
+            address   <= IR;
+            write_mem <= 1;
         end else begin
-            address   = {DATA_WIDTH{1'hz}};
-            write_mem = 0;
+            address   <= {DATA_WIDTH{1'hz}};
+            write_mem <= 0;
         end
     end
 
@@ -138,18 +135,22 @@ module Processor #(
 
     ////////////////////////  CPU reset Program Counter ////////////////////////////
 
-    always_comb begin
+    always_ff @(posedge clk) begin
         case (PC_Sel)
-            0: next_pc = PC + 1'b1;
-            1: next_pc = IR;
+            0: next_pc <= PC + 1'b1;
+            1: next_pc <= Z_flag ? IR : PC + 1'b1;
+            2: next_pc <= (!Z_flag) ? IR : PC + 1'b1;
+            3: next_pc <= (C_flag) ? IR : PC + 1'b1;
+            4: next_pc <= (!C_flag) ? IR : PC + 1'b1;
             default: begin
             end
         endcase
     end
 
     always @(posedge clk) begin
-        if (reset_n) PC <= next_pc;
-        else PC <= {ADDR_WIDTH{1'b0}};
+        if (!reset_n) PC <= {ADDR_WIDTH{1'b0}};
+        else PC <= next_pc;
+
     end
 
     //////////////////////////////////////////////////////////////////////
